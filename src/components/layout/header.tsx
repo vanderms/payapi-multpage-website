50001831b4d385e6646c2132293c78bd823e0a77
vanderms <vanderms.84@gmail.com>
2022-09-03 13:07:10 -0300

    feat: implement navbar mobile design

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,77 @@
-export const Header: React.FC = () => { 
+import data from "@/data/content/layout.json";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export const Header: React.FC = () => {
+  const [sidebar, setSidebar] = useState<{ open: boolean }>({ open: false });
+  const location = useLocation();
+
+  useEffect(() => {
+    setSidebar({ open: false });
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
-    <div>
-      Header works!
-    </div>
+    <header>
+      <div className="sr-only">
+        <h1>{data.header.title}</h1>
+        <a href="#main">Skip Nav</a>
+      </div>
+      <nav className="mt-10">
+        <div className="container flex items-center justify-between">
+          <Link to="/" aria-label="Homepage">
+            <img src={data.header.assets.logo} alt="" />
+          </Link>
+          <button
+            aria-label="open menu"
+            className="py-1 px-2"
+            onClick={() => {
+              setSidebar({ open: true });
+            }}
+          >
+            <img src={data.header.assets.menu} alt="" />
+          </button>
+          {sidebar.open && (
+            <div
+              className="fixed inset-0 z-[99998]"
+              onClick={() => {
+                setSidebar({ open: false });
+              }}
+            ></div>
+          )}
+          <div
+            className={`fixed top-0 bg-neutral-900 h-screen w-[min(18.75rem,100%)] pt-12 px-6 flex flex-col
+              z-[99999]
+              ${sidebar.open ? "right-0 transition-all" : "right-[-18.75rem]"}
+            `}
+          >
+            <button
+              aria-label="close menu"
+              className="py-1 px-2 self-end"
+              onClick={() => {
+                setSidebar({ open: false });
+              }}
+            >
+              <img src={data.header.assets.close} alt="" />
+            </button>
+            <div className="mt-6 w-full h-0 border-b border-neutral-100/20"></div>
+            <ul className="mt-11 flex flex-col items-center gap-8">
+              {data.header.links.map((link, i) => (
+                <li key={i}>
+                  <Link to={link.to} className="navbar-text">
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-8 w-full">
+              <Link to={data.header.cta.to} className="primary-button">
+                {data.header.cta.text}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
